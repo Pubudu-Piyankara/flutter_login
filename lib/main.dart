a93b0bac1804526257ed13479f1f0c516e194489
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-01-08 00:14:57 +0530

    Add  project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/Widgets/text_field.dart';
+import 'package:passwordfield/passwordfield.dart';
 
 //main fuction
 void main() {
@@ -18,7 +19,6 @@ class _MyAppState extends State<MyApp> {
   final myController = TextEditingController();
   // Color labelColor = Colors.black.withOpacity(0.5);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,15 +29,13 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            
-              Padding(
+            Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
                   controller: myController,
                   style: TextStyle(
                     color: Colors.white,
                   ),
-                    
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Username',
@@ -45,23 +43,42 @@ class _MyAppState extends State<MyApp> {
                       fillColor: Color.fromARGB(136, 91, 90, 90),
                       labelStyle: TextStyle(
                         color: Colors.white,
-                          )
-                      
-                          ),
-                )
-                )
-            ,
+                      )),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: PasswordField(
+                color: Colors.white,
+                passwordDecoration: PasswordDecoration(
+                  inputStyle: TextStyle(
+                    color: Colors.white,
+                  )
+                ),
+                border: PasswordBorder(
+                  border: OutlineInputBorder(),
+                ),
+                hintText: 'Password',
+                backgroundColor: Color.fromARGB(136, 91, 90, 90),
+                errorMessage: 'Error',
+              ),
+            ),
             TextButton(
               style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 0, 0, 0)),
+                backgroundColor: MaterialStateProperty.all<Color?>(Colors.yellow),
               ),
               onPressed: () {
                 print(myController.text);
                 setState(() {});
               },
-              child: Text('TextButton'),
+              child: Text('LogIn'),
             ),
-            Text(myController.text)
+            Text(
+              myController.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            )
           ],
         ),
       ),
